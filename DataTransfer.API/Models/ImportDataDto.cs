ae7167b4849d093182919b4146a6ce4c1f3d4068
Prabaharan P <praba.btech77@gmail.com>
2025-05-09 15:33:49 +0530

    save api

--- DataTransfer.API/Models/ImportDataDto.cs
+++ DataTransfer.API/Models/ImportDataDto.cs
@@ -15,8 +15,8 @@ namespace DataTransfer.API.Models
         public string SourceColumnList { get; set; }
         public string DescColumnList { get; set; }
         public string Description { get; set; }
-        public bool IsTruncate { get; set; }
-        public bool IsDeleteAndInsert { get; set; }
+        public string IsTruncate { get; set; }
+        public string IsDeleteAndInsert { get; set; }
         public string BeforeQuery { get; set; }
         public string AfterQuery { get; set; }
         public DateTime? CreatedDate { get; set; } = DateTime.Now;
